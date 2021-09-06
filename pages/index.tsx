4ddb4c7a18de0ea218156f05af5bb255ef53bce4
Matthew Spector <matt.bspector@yahoo.com>
2021-09-05 18:54:20 -0700

    Bookends

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,17 +36,36 @@ const IndexPage = ({ robes, lastUpdate }: Props) => {
     <div className="py-3 md:pb-0 font-mono flex flex-col justify-center items-center gap-4 pt-10 md:w-screen">
       <h1 className="text-lg md:text-3xl">The N Project Market</h1>
       <div className="text-center max-w-screen-md md:leading-loose">
-      <h2 className="text-lg pt-3 mv-4">Special</h2>
+      <h2 className="text-lg mv-4">Special</h2>
                          <p className="md:text-lg pt-2">
                     <a
             href="/club-69"
             className="underline"
           >
-           Club 69 (101/8888)
+           🔥 Club 69 🔥 (101/8888) 
           </a>
 
         </p>
-        <h2 className="text-lg mv-4">Individuals</h2>
+
+        <p className="md:text-lg pt-2">
+          <a
+            href="/bookends"
+            className="underline"
+          >
+           📚 Bookends 📚 (9/8888)
+          </a>
+          </p>
+
+          <p style={{color: '#ff5c5c'}} className="md:text-lg pt-2">
+                    <a
+            href="/primes"
+            className="underline"
+          >
+           🔑 Pure Primes 🔑 (9/8888)
+          </a>
+
+        </p>
+        <h2 className="text-lg pt-3 mv-4">Individuals</h2>
 
         <p className="md:text-lg pt-2">
           <a
@@ -141,18 +160,7 @@ const IndexPage = ({ robes, lastUpdate }: Props) => {
           </a>
 
         </p>
-
-
-                <h2 className="text-lg pt-3 mv-4">Sets</h2>
-                         <p style={{color: '#ff5c5c'}} className="md:text-lg pt-2">
-                    <a
-            href="/primes"
-            className="underline"
-          >
-           Pure Primes (9/8888)
-          </a>
-
-        </p>
+                         
 
         <h2 className="text-lg pt-3 mv-4">Scatter</h2>
                          <p className="md:text-lg pt-2">
