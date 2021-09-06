bbe0e91d4f4fab7861c3a262717527e4a103ebdc
Matthew Spector <matt.bspector@yahoo.com>
2021-09-05 19:36:52 -0700

    Donations

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,13 @@ const Robe = ({ robe }: { robe: RobeInfo }) => {
 
 const IndexPage = ({ robes, lastUpdate }: Props) => {
   return (
-    <div className="py-3 md:pb-0 font-mono flex flex-col justify-center items-center gap-4 pt-10 md:w-screen">
+    <div className="relative py-3 md:pb-0 font-mono flex flex-col justify-center items-center gap-4 pt-10 md:w-screen">
+      <div className="absolute top-2.5 left-2.5 text-xs">
+        <div>Donations will be used to sweep the floor 🧹🧹🧹</div>
+        <div>@mattyb: 0x70BFA29ACA546E6cFDc7a8F7Aebf07d9a545Cf52</div>
+        <div>@vanillagorilla: 0x2eD2C431b77A021Dffe03D7e8d8dDE481bb07cCB</div>
+
+      </div>
       <h1 className="text-lg md:text-3xl">The N Project Market</h1>
       <div className="text-center max-w-screen-md md:leading-loose">
       <h2 className="text-lg mv-4">Special</h2>
