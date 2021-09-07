4facd77914f97bf8692198607b9f095b6b0d4fa1
Nick Erokhin <nickerokhin@gmail.com>
2021-09-07 01:41:29 -0700

    oops

--- pages/sumgt70/index.tsx
+++ pages/sumgt70/index.tsx
@@ -37,7 +37,7 @@ const IndexPage = ({ robes, lastUpdate }: Props) => {
       <h1 className="text-lg md:text-3xl">Sum(n) {">"} 70</h1>
       <div className="text-center max-w-screen-md md:leading-loose">
         <p className="md:text-xl">
-          There are {robes.length} N's for sale where Sum(n) > 70. The floor
+          There are {robes.length} N's for sale where Sum(n) {'>'} 70. The floor
           price is {robes[0].price} ETH.
         </p>
         <p className="text-sm mv-4">Last updated {ts(lastUpdate)}</p>
